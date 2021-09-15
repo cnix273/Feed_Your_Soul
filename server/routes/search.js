44ebde8ad05dc0e18c509a5b726d0ac68266f34f
Cameron Nix <camerondeverenix@gmail.com>
2021-09-15 15:18:35 -0700

    First push

--- server/routes/search.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-
-const search_api = require('../apis/search_api');
-const authCheck = require("../config/middleware/authCheck");
-
-router.post('/search', authCheck, search_api.contactSearch);
-
-module.exports = router;
