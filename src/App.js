e85b1fdc13707669e2024b573c7f103fa75459c2
Cameron Nix <camerondeverenix@gmail.com>
2021-09-29 22:46:54 -0700

    moved loadNotes function here, updated state values, and passed props to Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import axios from 'axios';
 
 import Home from "./components/Home"
 import About from "./components/About"
@@ -21,49 +22,97 @@ import ViewNote from "./components/ViewNote";
 // making sure things like the back button and bookmarks
 // work properly.
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
+export default class App extends Component {
 
-        <hr />
+  constructor(props) {
+    super(props);
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/newpost">
-              <NewNote />
-          </Route>
-          <Route path="/viewpostit">
-              <ViewNote />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    this.state = {
+        noteDate: "",
+        emotionalState: "",
+        physicalState: "",
+        lookingForwardTo: "",
+        food: ""
+    };
+  }
+
+  loadNotes = submitObject => {
+
+    axios.post('/apis/getnote', submitObject)
+        .then(function (data) {
+        console.log(data);
+        // if (data.data.success) {
+        //     this.props.authenticate();
+        //     this.setState({
+        //     redirectToReferrer: true
+        //     });
+        // } else {
+        //     alert(data.data.message);
+        // }
+        // }.bind(this)).catch(function (err) {
+        // console.log(err);
+        // });
+
+    this.setState({
+      noteDate: "",
+      emotionalState: "",
+      physicalState: "",
+      lookingForwardTo: "",
+      food: ""
+    });
+  })}
+
+  render() {
+      return (
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            {/*
+              A <Switch> looks through all its children <Route>
+              elements and renders the first one whose path
+              matches the current URL. Use a <Switch> any time
+              you have multiple routes, but you want only one
+              of them to render at a time
+            */}
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route exact path="/dashboard" render={props => 
+                <Dashboard
+                  {...props}
+                    noteDate={this.state.noteDate}
+                    emotionalState={this.state.emotionalState}
+                    physicalState={this.state.physicalState}
+                    lookingForwardTo={this.state.lookingForwardTo}
+                    food={this.state.food}
+                />}>
+              </Route>
+              <Route path="/newpost">
+                  <NewNote />
+              </Route>
+              <Route path="/viewpostit">
+                  <ViewNote />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
+  }
 }
